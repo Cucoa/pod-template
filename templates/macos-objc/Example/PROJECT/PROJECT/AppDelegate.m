7b8bf13f5a7f485b173af2b3843a859ee0edd0f9
Tea <673921021@qq.com>
2021-03-02 11:51:46 +0800

    macOS-objc
    添加一个mac-objc的模版

---
+++ templates/macos-objc/Example/PROJECT/PROJECT/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  PROJECT
+//
+//  Created by Borsam on 2021/3/2.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
---
+++ templates/macos-objc/Example/PROJECT/PROJECT/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  PROJECT
+//
+//  Created by Borsam on 2021/3/2.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
